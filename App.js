a9aaa9070cbbc1a49c454f9240552223ce91d1df
deu04287 <48381510+deu04287@users.noreply.github.com>
2020-11-11 22:06:18 +0900

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.default}>
+      <Text>hellooooooooooooo...........</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  default: {
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+    backgroundColor: 'white'
+  }
+});
+
+export default App;
